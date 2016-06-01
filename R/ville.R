9ab28364400f4e66a0aab2b1fb0d715ca2098037
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2016-06-01 10:55:51 +0200

    adding documentation, example and changing Factors to string

--- R/ville.R
+++ R/ville.R
@@ -1,3 +1,12 @@
+#'Get by name
+#'
+#'Takes a the name of a French commune and returns name, code Insee, postal code, area, latitude and longitude
+#'@param Name the name of a commune 
+#'@return a data.frame with names, postal code, INSEE code, surface, lat and long. 
+#'@export
+#'@examples
+#'getByName("Épinal")
+#'
 getByName <- function(Name) {
   Name <- chartr("éèëêÉÈËÊàÀçÇôoœoöÔOŒOÖuûùüúUÛÙÜÚîïÎÏ", "eeeeEEEEaAcCoooooOOOOOuuuuuUUUUUIIII", Name)
   url <- paste0("https://geo.api.gouv.fr/communes?nom=", Name, "&fields=nom,codeInsee,codesPostaux,centre,surface&format=json&geometry=centre")
@@ -38,7 +47,7 @@ getByName <- function(Name) {
           lat <- coord[2]
           long <- coord[1]
         }
-        objbis <- data.frame(name = city, codeInsee = codeInsee, codesPostaux = codesPostaux, surface = surface, lat=lat, long=long)
+        objbis <- data.frame(name = city, codeInsee = codeInsee, codesPostaux = codesPostaux, surface = surface, lat=lat, long=long, stringsAsFactors = FALSE)
         identity <- rbind(identity,objbis)
       }
       return(identity)
